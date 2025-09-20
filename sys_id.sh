f2a2136931eedc6ad63ed8722679ff75b475bed5
Elijah Beringuel <beringuel.elijah@gmail.com>
2025-09-20 18:06:57 -0400

    Week 1 script revised

--- sys_id.sh
+++ sys_id.sh
@@ -6,17 +6,16 @@
 
 echo -e "  Hostname:\t$(hostname)\n"
 
-# This section creates an array for each line stored in the variable ip_addr
-# before proceeding to apply a for loop for each line to print its index
-# along with the line itself which contains the ip address of the vm.
-# This is a dynamic code that scales with the number of addresses on the vm.
+# This section enumerates the IP addresses available in the system and stores 
+# them in a variable before processing the variable using a for-loop to print
+# each IP address in a line.
+
+echo "IP Addresses: "
 
 ip_addr=$(ip a | grep "inet " | awk '{print $2}')
 
-readarray -t addr_list <<< $ip_addr
-
-for addr_count in "${!addr_list[@]}"; do
-  echo "  IP Address $addr_count: ${addr_list[$addr_count]}"
+for addr in $ip_addr; do
+  echo "	$addr"
 done
 
 # The last section retrieves the vm's default gateway using the ip route
