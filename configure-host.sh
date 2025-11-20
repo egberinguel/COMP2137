34cd364193abfa402403cf70aa97fa7e6b2f0328
Elijah Beringuel <beringuel.elijah@gmail.com>
2025-11-20 16:28:52 -0500

    First finished draft, to be adjusted'

--- configure-host.sh
+++ configure-host.sh
@@ -30,13 +30,16 @@
 
 function display_help {
 	cat <<EOF
-${basename $0} [-h] [-name hostname] [-ip ip_address] [-hostentry hostentry]
-	-h		display help and exit
-	-name		configure hostname
-	-ip		configure system IP address
-	-hostentry	update system host entry
+$(basename $0) [-h | --help]  [-name hostname]  [-ip ip address]  [-hostentry hostname]
+	-h | --help	Display help and exit
+	-name		Configure hostname
+	-ip		Configure system IP address
+	-hostentry	Update system host entry
+	
 EOF
 }
+
+declare -a host_check
 
 function hostscheck {
 	if [ ! -f /etc/hosts ]; then
@@ -44,16 +47,37 @@ function hostscheck {
 		exit 1
 	else
 		# Add $ if not including mgmt entries
-		host_check=$(grep -E "server[0-9]+" /etc/hosts)
+		entry_check=$(grep -B 1 mgmt /etc/hosts | awk '{ print $2 }' | head -n 2)
+		unique_system_name=$(grep -B 1 mgmt /etc/hosts | awk '{ print $2 }' | head -n 1)
+		for names in $entry_check; do
+			host_check+=("$names")
+		done
 	fi
 }
 
 function netplancheck {
-	echo "netplan check"
+	if [ ! -f /etc/netplan/*.yaml ]; then
+		echo "There are no files existing in /etc/netplan/ ! Please ask for system administrator assistance."
+		exit 1
+	else
+		if [ $(ls -l /etc/netplan | wc -l) -gt 2 ]; then
+			echo "This script assumes there is only file in /etc/netplan"
+			echo "Exiting..."
+			exit 1
+		else
+			netplan_file="$(ls /etc/netplan)"
+			current_address=$(grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /etc/netplan/* | head -n 1)
+		fi
+	fi
 }
 
 function hostnamecheck {
-	# Must return a variable
+	if [ ! -f /etc/hostname ]; then
+		echo "/etc/hostname does not exist! Please ask for system administrator assistance."
+		exit 1
+	else
+		current_hostname=$(cat /etc/hostname)
+	fi
 }
 
 while [ $# -gt 0 ]; do
@@ -64,14 +88,18 @@ while [ $# -gt 0 ]; do
 			;;
 		-name )
 			hostscheck
-			if [ $host_check != $2 ]; then
-				sed -i.bak -e 's/server[0-9]\+/hostname/' /etc/hosts
+			
+			# Might need to change localhost instead
+			# Scenario: what if server1 is changed and ip address is changed?
+			if [ "$host_check" != "$2" ]; then
+				echo "Changing "
+				sed -i.bak "s/$unique_system_name/$2/" /etc/hosts
 			else
 				echo "Host entry is already set as $2"
 			fi
 			
 			hostnamecheck
-			if [ $hostname_check != $2 ]; then
+			if [ "$hostname_check" != "$2" ]; then
 				hostnamectl hostname $2
 			else
 				echo "Hostname is already set as $2"
@@ -80,15 +108,65 @@ while [ $# -gt 0 ]; do
 			;;
 		-ip )
 			netplancheck
+			if [ "$current_address" != "$2" ]; then
+				echo "Adjusting netplan configuration"
+				sed -i.bak "s/$current_address/$2/" $netplan_file
+				netplan apply 2> /etc/null
+				if [ $? -ne 0 ]; then
+					echo "netplan failed to apply settings!"
+					mv $netplan_file".bak" $netplan_file
+					exit 1
+				fi
+			else
+				echo "System netplan address is already set as $2"
+			fi
+			
 			hostscheck
+			if [ "$host_check" != "$2" ]; then
+				echo "Changing "
+				sed -i.bak "s/$unique_system_name/$2/" /etc/hosts
+			else
+				echo "Host entry is already set as $2"
+			fi
 			exit
 			;;
 		-hostentry )
 			hostscheck
+			address_check=$(grep mgmt /etc/hosts -B 1 | awk '{ print $1 }'| head -n 1)
+			if [[ "$2" != ^[0-9]+\.[0-9]\.[0-9]\.[0-9]+$ ]]; then
+				if [ "$host_check" != "$2" ]; then
+					echo "Changing hostname entry in /etc/hosts"
+					sed -i "s/$unique_system_name/$2/" /etc/hosts
+				else
+					echo "Host entry is already set as $2"
+				fi
+				
+				if [ "$address_check" != "$3" ]; then
+					echo "Changing address entry in /etc/hosts"
+					sed -i.bak "s/$address_check/$3/" /etc/hosts
+				else
+					echo "Address entry is already set as $3"
+				fi
+			else
+				if [ "$host_check" != "$3" ]; then
+					echo "Changing hostname entry in /etc/hosts"
+					sed -i "s/$unique_system_name/$2/" /etc/hosts
+				else
+					echo "Host entry is already set as $3"
+				fi
+				
+				if [ "$address_check" != "$2" ]; then
+					echo "Changing address entry in /etc/hosts"
+					sed -i.bak "s/$address_check/$3/" /etc/hosts
+				else
+					echo "Address entry is already set as $2"
+				fi
+			fi
 			exit
 			;;
 		* )
 			echo "Invalid argument: '$1'"
+			display_help
 			exit 1
 			;;
 	esac
@@ -96,4 +174,5 @@ while [ $# -gt 0 ]; do
 	shift 2
 done
 
-
+echo ""
+display_help
