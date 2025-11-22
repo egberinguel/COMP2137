b3d78f279bb900b56d6617c5ff52dd9b5e194a3a
Elijah Beringuel <beringuel.elijah@gmail.com>
2025-11-21 22:57:40 -0500

    first completed draft

---
+++ lab3.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# This script runs the configure-host.sh script from the current directory to modify 2 servers and update the local /etc/hosts file
+
+VERBOSE=false
+
+function display_help {
+	cat <<EOF
+$(basename $0) [OPTIONAL: -verbose]
+	-verbose	Script will print debugging messages about its progress
+	
+EOF
+}
+
+while [ $# -gt 0 ]; do
+	case "$1" in
+		-verbose )
+			VERBOSE=true
+		;;
+		
+		* )
+			echo "Invalid option"
+			echo ""
+			display_help
+			exit 1
+		;;
+	esac
+	shift
+done
+
+if [ -f ./configure-host.sh ]; then
+	machines=(server1 server2)
+	
+	if [ "$VERBOSE" = "true" ]; then
+		VERBOSE_OPT="-verbose"
+	fi
+
+	for container in ${machines[@]}; do
+		scp configure-host.sh remoteadmin@$container-mgmt:/root
+		
+		case $container in
+			server1 )
+				ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4 $VERBOSE_OPT
+				;;
+			server2 )
+				ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3 $VERBOSE_OPT
+				;;
+		esac
+	done
+	
+	./configure-host.sh -hostentry loghost 192.168.16.3
+	./configure-host.sh -hostentry webhost 192.168.16.4
+else
+	echo "Script does not exist in this directory!"
+	exit 1
+fi
+
+
